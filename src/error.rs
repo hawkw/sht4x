d5c0e803ef7f4ed60c5dd96f9bbd13a6fe49cbbd
Eliza Weisman <eliza@elizas.website>
2024-06-09 14:42:49 -0700

    Add `embedded-hal-async` support
    This commit adds support for the `embedded-hal-async` crate in addition
    to `embedded-hal`. I've done this by adding a separate `AsyncSht4x`
    type, based on the assumption that most projects won't need to use both
    the blocking `embedded-hal` traits and the `embedded-hal-async` traits
    at the same time, and providing `async fn` methods on a separate type
    with the same names as the blocking ones seemed a bit nicer than having
    one type that has both `fn measure` and `async fn measure_async` and so
    on.
    
    Support for `embedded-hal-async` is gated behind the
    `embedded-hal-async` feature flag, so the dependency is not enabled by
    default.
    
    Note that this branch depends on my PR #6, which updates this crate to
    use `embedded-hal` v1.0, and currently contains the commit from that
    change as well. Once #6 has merged, this branch will need to be rebased
    onto the main branch.
    
    It also depends on my upstream PR adding `embedded-hal-async` support to
    `sensirion-i2c-rs`, sensirion/sensirion-i2c-rs#30, which has been
    [merged], but hasn't been published to crates.io yet. Currently, this
    branch adds a Cargo `[patch]` to use a Git dep on `sensirion-i2c-rs`.
    So, this change cannot be released to crates.io until upstream publishes
    a new release of `sensirion-i2c-rs`. Hopefully they do that soon! :)
    
    [merged]: https://github.com/Sensirion/sensirion-i2c-rs/commit/f7b9f3a81b777bc6e6b2f0acb4c1ef9c57dfa06d

--- src/error.rs
+++ src/error.rs
@@ -13,7 +13,7 @@ pub enum Error<E> {
 
 impl<I> From<i2c::Error<I>> for Error<I::Error>
 where
-    I: embedded_hal::i2c::I2c,
+    I: embedded_hal::i2c::ErrorType,
 {
     fn from(err: i2c::Error<I>) -> Self {
         match err {
